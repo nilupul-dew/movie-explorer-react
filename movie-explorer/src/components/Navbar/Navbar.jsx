511da8fbc4615e947c2c5a312276e1ef1b642442
W.A.S.C.N. Dewmini Ariyarathne <nilupul1025@gmail.com>
2025-05-11 05:52:17 +0530

    dark mode light mode

--- movie-explorer/src/components/Navbar/Navbar.jsx
+++ movie-explorer/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,12 @@
 import React, { useState } from 'react';  // Add useState import
 import './Navbar.css'
+import DarkMode from "../DarkMode/DarkMode"
 import Fire from '../../assets/fire.png'
 import Star from '../../assets/glowing star.png'
 import Party from '../../assets/upcoming.png'
 
 
   const Navbar = ({ setSearchQuery }) => {
-    const [inputValue, setInputValue] = useState('');
-
-  // Handle search query change
-  const handleSearch = (e) => {
-    setInputValue(e.target.value);  // Update inputValue state
-    setSearchQuery(e.target.value);  // Pass the value to the parent component (App.jsx)
-  };
 
   return  <nav className='navbar'> <h1>Movie Explorer</h1> 
     {/* Search bar */}
@@ -25,6 +19,7 @@ import Party from '../../assets/upcoming.png'
       />*/}
 
     <div className="navbar_links">
+      <DarkMode/>
         <a href="">Popular <img src={Fire} alt="Fire emoji" className='align_center navbar_emoji' /> </a>
         <a href="">Top Rated <img src={Star} alt="Star emoji" className='align_center navbar_emoji' /> </a>
         <a href="">Upcoming <img src={Party} alt="Party emoji" className='align_center navbar_emoji' /> </a>
